6d51990a6b97f4a78c91661f52700027284725ea
Sridhar <singupurapusridhar570@gmail.com>
2024-05-19 22:34:25 +0530

    Built Weather dashboard

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "be654c5ae6msh14a05abe13493c8p12d00bjsne2b22eb03861",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+    "Content-Type": "application/json",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "30240b42f42bf2bbcabd6b174722ab31";
